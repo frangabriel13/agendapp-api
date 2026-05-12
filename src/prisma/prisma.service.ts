0b5b3875272271a9be1a51c4a9a31f427d8c84ce
frangabriel13 <mansilla.franco.1@gmail.com>
2026-05-12 21:37:22 +0000

    cambios

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not set');
+    }
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Connected to database');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
